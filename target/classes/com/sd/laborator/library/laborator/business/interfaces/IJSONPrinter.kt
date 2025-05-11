bb0241cd484e9d9c2ef1348157a088142d21d03a
vektor8 <victor.mosolea@proton.me>
2025-05-11 19:32:19 +0300

    fixed

--- target/classes/com/sd/laborator/library/laborator/business/interfaces/IJSONPrinter.kt
+++
@@ -1,7 +0,0 @@
-package com.sd.laborator.library.laborator.business.interfaces
-
-import com.sd.laborator.library.laborator.business.models.Book
-
-interface IJSONPrinter {
-    fun printJSON(books: Set<Book>): String
-}
